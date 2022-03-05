0d7798e08b4147b85713424eb3bfccb1401c46fd
dhksgml <dhksgml05301@naver.com>
2022-03-05 17:24:03 +0900

    update 0305

--- 접속/clUDP.js
+++ 접속/clUDP.js
@@ -1,16 +1,67 @@
 var PORT = 9000;
 var HOST = '127.0.0.1';
 var dgram = require('dgram');
-var message = JSON.stringify({cmd:"CS", id:"asdf", pos: [1.7, 2.3, 5.5]});
+var CS = JSON.stringify({ cmd: "CS", id: "asdf" });
+var DBS = JSON.stringify({ cmd: "DBs", nick: "kk" })
+var CLO = JSON.stringify({ cmd: "cLo", nick: "kk" })
+var MAO = JSON.stringify({ cmd: "mAo", mID: "11" })
 
 var client = dgram.createSocket('udp4');
-client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+client.send(CS, 0, CS.length, PORT, HOST, function(err, bytes) {
     if (err) throw err;
     console.log('UDP message send to' + HOST +':'+ PORT);
-    // console.log()
-    // client.close();
 });
 
 client.on('message',function(msg,rinfo){
     console.log(JSON.parse(msg.toString()));
-})
+    const udbin = JSON.parse(msg.toString())
+    if (udbin.cmd == 'uDB') {
+        client.send(DBS, 0, DBS.length, PORT, HOST, function(err, bytes) {
+            if (err) throw err;
+            console.log('UDP message send to' + HOST + ":" + PORT);
+        });
+    }
+    const cloin = JSON.parse(msg.toString());
+    if (cloin.cmd == "cLs") {
+        client.send(CLO, 0, CLO.length, PORT, HOST, function(err, bytes) {
+            if (err) throw err;
+            console.log('UDP message send to' + HOST + ":" + PORT);
+        });
+    }
+    const maoin = JSON.parse(msg.toString());
+    if (maoin.cmd == "mAs") {
+        client.send(MAO, 0, MAO.length, PORT, HOST, function(err, bytes) {
+            if (err) throw err;
+            console.log('UDP message send to' + HOST + ":" + PORT);
+        });
+    }
+});
+
+// client.on('message',function(msg,rinfo){
+//     console.log(JSON.parse(msg.toString()));
+// })
+
+// client.send(UDB, 0, UDB.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message send to' + HOST +':'+ PORT);
+//     // console.log()
+//     // client.close();
+// });
+
+// client.send(CLS, 0, UDB.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message send to' + HOST +':'+ PORT);
+//     // console.log()
+//     // client.close();
+// });
+
+// client.send(MAS, 0, UDB.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message send to' + HOST +':'+ PORT);
+//     // console.log()
+//     // client.close();
+// });
+
+// var UDB = JSON.stringify({cmd:"uDB",  nick: "asdf", Lx: "5435", Ly: "213", Lz: "3454", Rz: "9809", cla: "1", lv: "1", pExp: "8", act: "31"});
+// var CLS = JSON.stringify({cmd:"cLs",  nick: "asdf", Lx: "5435", Ly: "213", Lz: "3454", Rz: "9809", cla: "1", lv: "1", pExp: "8", act: "31"});
+// const MAS = JSON.stringify({cmd: "mAs", mID: "11", mcHP: "100", cLx: "-2400", cLy: "-9510", cLz: "-111.6", cRz: "90", mAct: "101", mRT: "10000"})
