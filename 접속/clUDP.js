b6a986dce34770eae00c112ef66a0475fd35e101
seunga <wm84358830@gmail.com>
2022-03-07 22:46:07 +0900

    0307

--- 접속/clUDP.js
+++ 접속/clUDP.js
@@ -3,48 +3,66 @@ var HOST = '127.0.0.1';
 var dgram = require('dgram');
 var CS = JSON.stringify({ cmd: "CS", id: "asdf" });
 var DBS = JSON.stringify({ cmd: "DBs", nick: "kk" })
-var CLO = JSON.stringify({ cmd: "cLo", nick: "kk" });
-var MAO = JSON.stringify({ cmd: "mAo", mID: "11" });
-var GG = JSON.stringify({ cmd: "GG", nick: "kk"});
-var CN = JSON.stringify({ cmd: "CN", nick: "kk", CNt: 20});
+var CLO = JSON.stringify({ cmd: "cLo", nick: "kk" })
+var MAO = JSON.stringify({ cmd: "mAo", mID: "11" })
+var CC = JSON.stringify({cmd: "CC", id: "gj1", cla: 1, pAtt: 1, pmHP: 10});
 
 var client = dgram.createSocket('udp4');
-// client.send(GG, 0, GG.length, PORT, HOST, function(err, bytes) {
-//     if (err) throw err;
-//     console.log('UDP message send to' + HOST +':'+ PORT);
-// });
-
-setInterval(()=>{
-    client.send(CN, 0, CN.length, PORT, HOST, function(err, bytes) {
-        if (err) throw err;
-        console.log('UDP message send to' + HOST +':'+ PORT);
-    });
-}, 20000);
+client.send(CC, 0, CC.length, PORT, HOST, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message send to' + HOST +':'+ PORT);
+});
 
 client.on('message',function(msg,rinfo){
-    const getInfo = JSON.parse(msg.toString())
-    if (getInfo.cmd == 'uDB') {
+    console.log(JSON.parse(msg.toString()));
+    const udbin = JSON.parse(msg.toString())
+    if (udbin.cmd == 'uDB') {
         client.send(DBS, 0, DBS.length, PORT, HOST, function(err, bytes) {
             if (err) throw err;
             console.log('UDP message send to' + HOST + ":" + PORT);
         });
     }
-    //const cloin = JSON.parse(msg.toString());
-    if (getInfo.cmd == "cLs") {
+    const cloin = JSON.parse(msg.toString());
+    if (cloin.cmd == "cLs") {
         client.send(CLO, 0, CLO.length, PORT, HOST, function(err, bytes) {
             if (err) throw err;
             console.log('UDP message send to' + HOST + ":" + PORT);
         });
     }
-    //const maoin = JSON.parse(msg.toString());
-    if (getInfo.cmd == "mAs") {
+    const maoin = JSON.parse(msg.toString());
+    if (maoin.cmd == "mAs") {
         client.send(MAO, 0, MAO.length, PORT, HOST, function(err, bytes) {
             if (err) throw err;
             console.log('UDP message send to' + HOST + ":" + PORT);
         });
     }
-    if(getInfo.cmd == "GG")
-    {
-        console.log(getInfo.cmd);
-    }
 });
+
+// client.on('message',function(msg,rinfo){
+//     console.log(JSON.parse(msg.toString()));
+// })
+
+// client.send(UDB, 0, UDB.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message send to' + HOST +':'+ PORT);
+//     // console.log()
+//     // client.close();
+// });
+
+// client.send(CLS, 0, UDB.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message send to' + HOST +':'+ PORT);
+//     // console.log()
+//     // client.close();
+// });
+
+// client.send(MAS, 0, UDB.length, PORT, HOST, function(err, bytes) {
+//     if (err) throw err;
+//     console.log('UDP message send to' + HOST +':'+ PORT);
+//     // console.log()
+//     // client.close();
+// });
+
+// var UDB = JSON.stringify({cmd:"uDB",  nick: "asdf", Lx: "5435", Ly: "213", Lz: "3454", Rz: "9809", cla: "1", lv: "1", pExp: "8", act: "31"});
+// var CLS = JSON.stringify({cmd:"cLs",  nick: "asdf", Lx: "5435", Ly: "213", Lz: "3454", Rz: "9809", cla: "1", lv: "1", pExp: "8", act: "31"});
+// const MAS = JSON.stringify({cmd: "mAs", mID: "11", mcHP: "100", cLx: "-2400", cLy: "-9510", cLz: "-111.6", cRz: "90", mAct: "101", mRT: "10000"})
